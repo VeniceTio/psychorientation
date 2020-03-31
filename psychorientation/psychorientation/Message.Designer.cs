cdfc7a73e43b80ef29b9c2c83a6e296c3e73dc67
Antoine Tritschberger <antoine.tritschberger@etu.unistra.fr>
2020-03-31 17:22:22 +0200

    Ajout des eleves et d'un affichage global

---
+++ psychorientation/psychorientation/Message.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace psychorientation
+{
+    partial class Message
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblTitre = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Location = new System.Drawing.Point(99, 93);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(547, 160);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "label1";
+            // 
+            // lblTitre
+            // 
+            this.lblTitre.AutoSize = true;
+            this.lblTitre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitre.Location = new System.Drawing.Point(133, 24);
+            this.lblTitre.Name = "lblTitre";
+            this.lblTitre.Size = new System.Drawing.Size(66, 24);
+            this.lblTitre.TabIndex = 1;
+            this.lblTitre.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Accepter";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(456, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 51);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Refuser";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Message
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 338);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblTitre);
+            this.Controls.Add(this.lblMessage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Message";
+            this.Text = "Message";
+            this.Load += new System.EventHandler(this.Message_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblTitre;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ psychorientation/psychorientation/eleve.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psychorientation
+{
+    class eleve
+    {
+        private string classe = "";
+        private double competence = 0;
+        private double effort = 0;
+        private string orientation = "";
+
+
+        public eleve()
+        {
+            this.classe = "seconde";
+            this.competence = 10;
+            this.effort = 10;
+            this.orientation = "Inconnu";
+        }
+
+        public eleve(string classe, double competence, double effort, string orientation)
+        {
+            this.classe = classe;
+            this.competence = competence;
+            this.effort = effort;
+            this.orientation = orientation;
+        }
+
+        public void setClasse(string classe)
+        {
+            this.classe = classe;
+        }
+
+        public void setOrientation(string orientation)
+        {
+            this.orientation = orientation;
+        }
+
+        public void setEffort(double effort)
+        {
+            this.effort = effort;
+        }
+
+        public void setCompetence(double competence)
+        {
+            this.competence = competence;
+        }
+
+
+        public double getCompetence()
+        {
+            return this.competence;
+        }
+        public string getClasse()
+        {
+            return this.classe;
+        }
+        public string getOrientation()
+        {
+            return this.orientation;
+        }
+        public double getEffort()
+        {
+            return this.effort;
+        }
+    }
+}
