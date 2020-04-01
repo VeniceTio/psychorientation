a326c81620a963d19ca1a22374ed71e1e69da1e4
Loïc Gross <loic.gross@unistra.fr>
2020-04-01 14:50:43 +0200

    début form ListeEleve

---
+++ psychorientation/psychorientation/ListeEleve.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace psychorientation
+{
+    public partial class ListeEleve : Form
+    {
+        public ListeEleve()
+        {
+            InitializeComponent();
+        }
+
+        private void ListeEleve_Load(object sender, EventArgs e)
+        {
+            GestionnaireEleve gestEleve = new GestionnaireEleve();
+            List<Eleve> listEleve = gestEleve.getListEleve();
+
+            foreach ( Eleve eleve in listEleve)
+            {
+                InterfaceInfoEleve ii = new InterfaceInfoEleve();
+                ii.setParam(eleve, listeClasse);
+                pnlListeEleve.Controls.Add(ii);
+            }
+        }
+    }
+}
