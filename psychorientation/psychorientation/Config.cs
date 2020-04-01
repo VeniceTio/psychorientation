ad1f0bc2e8a69c2c4e542f8c468707e8552113ae
Antoine Félix <felixantoine67@yahoo.fr>
2020-04-01 13:27:05 +0200

    Ajout de NewtonSoft pour gerer les fichiers JSON ainsi que d une classe Config dans "Config.js".

---
+++ psychorientation/psychorientation/Config.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace psychorientation
+{
+    class Config
+    {
+        private static Config instance;
+
+        public double coeffEffortOral;
+        public double coeffCompetenceOral;
+
+        public double coeffCompetenceEcrit;
+        public double coeffEffortEcrit;
+
+        public static Config getInstance()
+        {
+            if (Config.instance == null)
+            {
+
+                Config.instance = JsonConvert.DeserializeObject<Config>(File.ReadAllText("config.json"));
+                Console.WriteLine(Config.instance.coeffEffortOral);
+                return Config.instance;
+            }
+            else
+            {
+                return Config.instance;
+            }
+        }
+
+        public double getCoeffEffortOral()
+        {
+            return this.coeffEffortOral;
+        }
+
+        public double getCoeffCompetenceOral()
+        {
+            return this.coeffCompetenceOral;
+        }
+
+        public double getCoeffCompetenceEcrit()
+        {
+            return this.coeffCompetenceOral;
+        }
+
+        public double getCoeffEffortEcrit()
+        {
+            return this.coeffEffortEcrit;
+        }
+    }
+}
